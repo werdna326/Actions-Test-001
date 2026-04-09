10939a8fb0f2c6ae133614a32ac4481c3b7903ec
Actions Test User <test@example.com>
2026-04-09 22:03:18 +0900

    Add SAM application to test cache

---
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App implements RequestHandler<Object, Object> {
+
+    public Object handleRequest(final Object input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        return "{\"message\": \"hello world\", \"location\": \"GitHub Actions\"}";
+    }
+}
